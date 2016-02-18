197a06598cadd36d52ca6a801a305beb8f968769
tyzhangxj <tyzhangxj@gmail.com>
2016-02-18 13:44:50 +0800

    '3'

--- src/com/zhuzeyao/light/LightActivity.java
+++ src/com/zhuzeyao/light/LightActivity.java
@@ -1,6 +1,6 @@
 package com.zhuzeyao.light;
-
-import com.zhuzeyao.app.R;
+ 
+import com.zhuzeyao.light.R;
 
 import android.app.Activity;
 import android.hardware.Camera;
@@ -11,11 +11,11 @@ public class LightActivity extends Activity {
     /** Called when the activity is first created. */
 	Camera camera = Camera.open();  
     boolean islight=false; 
-    @Override
+    @Override  
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-          
+             
         if (!islight) {  
             Parameters mParameters = camera.getParameters();  
             mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);  
